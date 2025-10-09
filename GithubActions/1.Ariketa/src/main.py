33b718d3a8a249a55759894aa634ffea6dbb4a23
Josu <delaflor.josu@uni.eus>
2025-10-09 10:36:31 +0200

    Add DockerFile

--- GithubActions/1.Ariketa/src/main.py
+++ GithubActions/1.Ariketa/src/main.py
@@ -1,6 +1,12 @@
+def main():
+    print("Docker")
+
 class Kalkulagailua:
     
     def batuketa(self, x: float, y: float) -> float: 
         return x + y
+
+if __name__ == "__main__":
+    main()
 
     
